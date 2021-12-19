345ac6402534ebe88c9ac373287b86ce0b24586a
kytan1314 <kytan1314@gmail.com>
2021-12-19 04:31:59 +0000

    Commit 1

---
+++ src/routes/index.svelte
@@ -0,0 +1,22 @@
+<script>
+    let name = "";
+    let age = "";
+    
+    async function getAge() {
+  fetch(' https://api.agify.io/?name=' + name)
+	.then((response) => response.json())
+	.then((data) => {
+    name = data.name;
+  	age = data.age; // Path for temperature
+	});
+}
+</script>
+<h1>Guess Age</h1>
+
+<form on:submit|preventDefault={getAge}>
+    <label for="name">name:</label>
+    <input id="name" bind:value={name} type="text" />
+    <button type="submit">Check</button>
+</form>
+
+<p>Age: {age}</p>
